70e9fe362d3dfbe0620182c8f8e03ef9e3606876
pxai <pello_altadill@cuatrovientos.org>
2017-07-07 08:48:42 +0200

    Added simple clase with many responsibilities

--- src/main/java/io/pello/cleancode/scenarios/classes/wrong/Weapon.java
+++ src/main/java/io/pello/cleancode/scenarios/classes/wrong/Weapon.java
@@ -1,5 +1,30 @@
 package io.pello.cleancode.scenarios.classes.wrong;
 
-public class Weapon {
+import java.util.ArrayList;
 
+public class Weapon {
+	private int damage;
+	private ArrayList<Character> killedEnemies;
+	
+	public Weapon(int damage) {
+		this.damage = damage;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+	
+	public String killedEnemiesReport () {
+		return "";
+	}
+	
+	public float sliceSomeBread () {
+		return 0;
+	}
+	
+	
 }
