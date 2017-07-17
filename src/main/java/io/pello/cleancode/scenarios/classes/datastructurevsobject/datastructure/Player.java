743daaa3676a1b3fe7f809edecbb52bfecbe1425
pxai <pello_altadill@cuatrovientos.org>
2017-07-17 20:07:13 +0200

    Ok, added changes

--- src/main/java/io/pello/cleancode/scenarios/classes/datastructurevsobject/datastructure/Player.java
+++ src/main/java/io/pello/cleancode/scenarios/classes/datastructurevsobject/datastructure/Player.java
@@ -23,107 +23,68 @@ public class Player {
 		this.position = position;
 	}
 
-	/**
-	 * @return the name
-	 */
+
 	public String getName() {
 		return name;
 	}
 
-	/**
-	 * @param name the name to set
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	/**
-	 * @return the strength
-	 */
 	public int getStrength() {
 		return strength;
 	}
 
-	/**
-	 * @param strength the strength to set
-	 */
+
 	public void setStrength(int strength) {
 		this.strength = strength;
 	}
 
-	/**
-	 * @return the speed
-	 */
+
 	public int getSpeed() {
 		return speed;
 	}
 
-	/**
-	 * @param speed the speed to set
-	 */
 	public void setSpeed(int speed) {
 		this.speed = speed;
 	}
 
-	/**
-	 * @return the intelligence
-	 */
 	public int getIntelligence() {
 		return intelligence;
 	}
 
-	/**
-	 * @param intelligence the intelligence to set
-	 */
+
 	public void setIntelligence(int intelligence) {
 		this.intelligence = intelligence;
 	}
 
-	/**
-	 * @return the type
-	 */
 	public int getType() {
 		return type;
 	}
 
-	/**
-	 * @param type the type to set
-	 */
 	public void setType(int type) {
 		this.type = type;
 	}
 
-	/**
-	 * @return the life
-	 */
 	public int getLife() {
 		return life;
 	}
 
-	/**
-	 * @param life the life to set
-	 */
 	public void setLife(int life) {
 		this.life = life;
 	}
 
-	/**
-	 * @return the position
-	 */
 	public Position getPosition() {
 		return position;
 	}
 
-	/**
-	 * @param position the position to set
-	 */
+
 	public void setPosition(Position position) {
 		this.position = position;
 	}
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
+
 	@Override
 	public String toString() {
 		return "Player [name=" + name + ", strength=" + strength + ", speed=" + speed + ", intelligence=" + intelligence
